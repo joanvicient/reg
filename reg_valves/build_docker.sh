7ca9e3777857bc0e30d044263b7f2c267ee3fc85
Joan Vicient <lo.roquetes@gmail.com>
2024-05-17 15:22:57 +0200

    - impreved build comments in reg_valves

--- reg_valves/build_docker.sh
+++ reg_valves/build_docker.sh
@@ -9,3 +9,6 @@ docker build -t jvicient/reg_valves -f reg_valves/Dockerfile .
 
 # run it executing:
 # docker run -p 5001:5001 jvicient/reg_valves
+#
+# push
+# docker push jvicient/reg_valves:latest
