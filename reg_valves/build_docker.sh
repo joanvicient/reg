c67464290688d2b6b56630a5cb7065d873876748
Joan Vicient <lo.roquetes@gmail.com>
2024-05-23 19:31:20 +0200

    - releases reg_valves:v1.0 and reg_telegram:v1.0

--- reg_valves/build_docker.sh
+++ reg_valves/build_docker.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+# Comprovar que hi ha exactament un paràmetre
+if [ "$#" -ne 1 ]; then
+    echo "Es requereix exactament un paràmetre."
+    exit 1
+fi
+
+# Obtenir el paràmetre
+versio="$1"
+
+# Comprovar que el paràmetre només conté números, lletres o '.'
+if [[ "$versio" =~ ^[a-zA-Z0-9.]+$ ]]; then
+    echo "Creant versió: "$versio
+else
+    echo "El paràmetre no és vàlid. Només es permeten números, lletres o '.'."
+    exit 1
+fi
+
 cd "$(dirname "$0")"
 cd ..
 
 ls -la
 
-docker build -t jvicient/reg_valves -f reg_valves/Dockerfile .
+docker build -t jvicient/reg_valves:latest -t jvicient/reg_valves:$versio -f reg_valves/Dockerfile .
 
 # run it executing:
 # docker run -p 5001:5001 jvicient/reg_valves
-#
+
 # push
-# docker push jvicient/reg_valves:latest
+echo ""
+echo "docker push jvicient/reg_valves:"$versio
+echo "docker push jvicient/reg_valves:latest"
